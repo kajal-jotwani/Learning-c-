559b19eefe6364f8c4ca1137e9e151939c5557ae
kajal-jotwani <kajaljotwani06@gmail.com>
2023-09-30 10:19:48 +0530

    learning

---
+++ if_else/learn_html/pattern pritning/learn c++/evennumbers.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int n;
+cin>>n;
+ int i=2;
+  int sum=0;
+ while(i<=n){
+    if(i%2==0)
+    sum=sum+i;
+    i=i+1;
+ } 
+ cout<<"sum from"<<1<<"to"<<n<<"="<<sum<<endl;
+ return 0;
+} 
