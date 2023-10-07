00cfc7b4e46e121d3acb7c912ab381fbc9f03abc
kajal-jotwani <kajaljotwani06@gmail.com>
2023-10-07 10:40:55 +0530

    learning

---
+++ if_else/learn_html/pattern pritning/learn c++/pyramid_star.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int row, col;
+    int n;
+    cout << "ENTER A NUMBER:" ; 
+    cin >> n;
+
+    for (row = 1; row <= n; row = row + 1)
+    {
+        for (col = 1; col <= n - row; col = col + 1)
+        {
+            cout << " ";
+        }
+
+        for (col = 1; col <= (2 * row) - 1; col = col + 1)
+        {
+            cout << "*";
+        }
+
+        cout << endl;
+    }
+}
