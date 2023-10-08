405fe663e6ad2a9717e9c1c40d1db5eaeb2895c0
kajal-jotwani <kajaljotwani06@gmail.com>
2023-10-08 22:35:11 +0530

    LEARNING

---
+++ if_else/learn_html/pattern pritning/learn c++/switch.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+ int main()
+ {
+    int i;
+    cout<<"ENTER THE NUMBER";
+    cin>>i;
+
+    switch(i)
+    {
+        case 1:
+        cout<<"KAJAL";
+        break;
+
+        case 2:
+        cout<<"TANU";
+        break;
+
+        default:
+        cout<<"KAJU";
+    }
+ }
