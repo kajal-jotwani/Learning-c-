77af1b89077c4767a124ffdd8d8b7929c6124758
kajal-jotwani <kajaljotwani06@gmail.com>
2023-10-14 23:32:11 +0530

    learning

---
+++ if_else/learn_html/pattern pritning/learn c++/switch2.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+ int main()
+ {
+    int i;
+    cout<<"ENTER THE NUMBER";
+    cin>>i;
+
+    switch(i)
+    {
+        case 1:
+        cout<<"maths";
+        break;
+
+        case 2:
+        cout<<"science";
+       break;
+
+        default:
+        cout<<"social";
+    }
+ }
